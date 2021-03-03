1819f4aeba29947fe0b03806b480082b4078bea7
Oops61rus <antodeni@rambler.ru>
2021-03-03 23:22:11 +0300

    1:
    create track.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import {Module} from "@nestjs/common";
+import {TrackModule} from "./track/track.module";
 
 
 @Module({
-    controllers: [],
-    providers: []
+    imports: [TrackModule]
 })
 
 export class AppModule {}
